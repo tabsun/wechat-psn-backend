6e5c7fbd9ed5036dd2fa677f9d3a02c1425c6455
Tabsun <buptmsg@gmail.com>
2017-02-09 22:32:12 +0800

    add image search and cover image

---
+++ BaiduImageSearch.py
@@ -0,0 +1,85 @@
+# -*- coding:utf-8 -*-
+#coding=utf-8
+from urllib import quote
+import urllib2 as urllib
+import random
+import re
+import os
+
+
+class BaiduImage():
+
+    def __init__(self, keyword, count=3, save_path="img", rn=60):
+        self.keyword = keyword
+        self.count = count
+        self.save_path = save_path
+        self.rn = rn
+
+        self.__imageList = []
+
+        self.__encodeKeyword = quote(self.keyword)
+        self.__acJsonCount = self.__get_ac_json_count()
+
+        self.user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36"
+        self.headers = {'User-Agent': self.user_agent, "Upgrade-Insecure-Requests": 1,
+                        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                        "Accept-Encoding": "gzip, deflate, sdch",
+                        "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6",
+                        "Cache-Control": "no-cache"}
+
+    def search(self):
+        for i in range(0, self.__acJsonCount):
+            url = self.__get_search_url(i * self.rn)
+            response = self.__get_response(url)
+            image_url_list = self.__pick_image_urls(response)
+            self.__imageList.extend(image_url_list)
+
+    def get_images_url(self):
+        self.search()
+        num = random.randint(4, 6)
+        urls = []
+        count = 0
+        for image in self.__imageList:
+            host = self.get_url_host(image)
+            self.headers["Host"] = host
+            if count < num:
+                try:
+                    req = urllib.Request(image, headers=self.headers)
+                    img = urllib.urlopen(req, timeout=20)
+                    real_url = img.geturl()
+                    real_url = real_url.replace('fm=23&gp=0.jpg','fm=21&gp=0.jpg')
+                    urls.append(real_url)
+                    count += 1
+                except Exception as e:
+                    count += 0
+            else:
+                break
+        return urls
+
+    def __pick_image_urls(self, response):
+        reg = r'"thumbURL":"(.*?)"'
+        imgre = re.compile(reg)
+        imglist = re.findall(imgre, response)
+        return imglist
+
+    def __get_response(self, url):
+        page = urllib.urlopen(url)
+        return page.read()
+
+    def __get_search_url(self, pn):
+        return "http://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&is=&fp=result&queryWord=" + self.__encodeKeyword + "&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=0&word=" + self.__encodeKeyword + "&s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&pn=" + str(pn) + "&rn=" + str(self.rn) + "&gsm=1000000001e&1486375820481="
+
+    def get_url_host(self, url):
+        reg = r'http://(.*?)/'
+        hostre = re.compile(reg)
+        host = re.findall(hostre, url)
+        if len(host) > 0:
+            return host[0]
+        return ""
+
+    def __get_ac_json_count(self):
+        a = self.count % self.rn
+        c = self.count / self.rn
+        if a:
+            c += 1
+        return c
--- index.py
+++ index.py
@@ -2,6 +2,7 @@
 import werobot
 import time
 from string import Template
+from BaiduImageSearch import BaiduImage
 
 # weixin server
 robot = werobot.WeRoBot(token='tabsunirumor', enable_session=True)
@@ -11,11 +12,41 @@ def subscribe(message):
 
 @robot.text
 def articles(message):
-    title_str = message.content
+    opinion_str = message.content  
+    opinion_str = opinion_str.encode('utf-8')
     date_str = time.strftime('%Y-%m-%d',time.localtime(time.time()))
-    title_str = title_str.encode('utf-8')
-    #date_str.encode('utf-8')
-    html_str1 ="""
+    # get article's title and content about the opinion
+    title_str = opinion_str
+    content_str = """《奇葩说》第二季里有一个辩题是：女生该不该追男生？\n
+        喵以为，这个辩题的要点在“怎么定义追”？如果采取大众定义，“追=表白+付出的行为”。
+        那么，在中国的语境下，我的答案是：不该！除非三个条件……\n
+        不过，喵喵君提供一个心理学精神分析学派的视角来看这个问题。逻辑链条有点长，
+        请智商100+的同学们慢慢follow，这还要从重男轻女的社会说起！\n\n
+        【重男轻女社会的悲剧轮回】\n
+        （地图炮预警！不过主要是对这三个省的情况比较熟悉，不代表这些省份的所有人都这样，
+        也不代表其他省份就不这样）\n
+        众所周知，中国（尤其大汉族沙文主义兴盛的东南三省），重男轻女仍十分严重（
+        闽赣和粤之潮汕地区三省的女孩子，向你们投去深深的同情，据我所知，这部分地区
+        依然男女分桌，不生男孩的女人会没地位，好强的女人会逼自己的孩子跟自己姓来传宗接代）。\n
+        重男轻女会有什么问题呢？\n\n
+
+        首先，这样的社会会标榜女人不求回报的付出，因为“你生为女人就有罪”；会要求女人“
+        精明能干/干活麻利”，因为女人天生低人一等，娶你为了服侍丈夫老爷的嘛！（我见过好多
+        福建的女孩特别能干上进，其实是对自己是女儿身的自卑的过度补偿）如果出现了“气管炎”，
+        在这部分地区是十分丢人和抬不起头的事情，会被整个社会耻笑。男人如果能有好几个女人，
+        是十分光荣的事情，女人要是出轨，就是十恶不赦天打雷劈——因为在这里女人最多男人价值的
+        0.5嘛，那必须1个男人占有2个以上女人，才符合价值定律啊！\n
+
+        于是，这就创造了一种可怕的代际轮回。"""
+    # get cover image(also top image)
+    cover_url = "https://secure.gravatar.com/avatar/0024710771815ef9b74881ab21ba4173?s=420"
+    image_searcher = BaiduImage(opinion_str)
+    images_url = image_searcher.get_images_url()
+    if len(images_url) > 0:
+        cover_url = images_url[0]
+        
+    img_insert_tpl = "<img src=%s /img>" % cover_url    
+    html_str ="""
         <html>
             <head>
             <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -23,19 +54,17 @@ def articles(message):
             </head>
 
             <body>
-            <h2>"""
-    html_str2 = """</h2>
+            <h2>%s</h2>
             <hr />
-            <p>"""
-    html_str3 = """ tabsun irumor</p>
+            <p>%s tabsun irumor</p>
             <br>
-            <p>Just for test</p>
+            %s
+            <p>%s</p>
             <br>
             <p>更多内容关注irumor</p>
             <img src="http://i.imgur.com/o8L9ItZ.jpg"/>
             </body>
-        </html>"""
-    html_str = html_str1 + title_str + html_str2 + date_str + html_str3
+        </html>""" % (title_str, date_str, img_insert_tpl, content_str)
 
     html_file= open("/var/ArticlePoolVolume/index.html","w")
     html_file.write(html_str)
@@ -43,9 +72,9 @@ def articles(message):
     
     return [
         [
-            "whtsky",
+            title_str,
             "I wrote WeRobot",
-            "http://img0.imgtn.bdimg.com/it/u=3324759509,3214419485&fm=23&gp=0.jpg",
+            cover_url,
             "http://tabsun-nginx-web.daoapp.io"
         ]
     ]
