05df187da131f20670bb3cf46b4459d98ca739a0
Tabsun <buptmsg@gmail.com>
2017-02-23 02:20:16 +0800

    fix bug

--- index.py
+++ index.py
@@ -131,8 +131,8 @@ def Generate(title, date, content, images):
         while content.find(split_flag) != -1 or image_cnt >= len(images):
             pre = content[:content.find(split_flag)+len(split_flag)]
             content = content[content.find(split_flag)+len(split_flag):]
-            pre = '<br><div align="center"><img src=' + images[image_cnt] + \
-                  'width="100%"/img></div><br><font face="微软雅黑" size="12">' + pre + '</font>'
+            pre = '<br><div align="center"><img src="' + images[image_cnt] + \
+                  '" width="100%"/img></div><br><font face="微软雅黑" size="12">' + pre + '</font>'
             cti = cti + pre
             image_cnt += 1
             if image_cnt >= len(images):
@@ -156,7 +156,8 @@ def Generate(title, date, content, images):
             %s
             <br>
             <font face="微软雅黑" size="12">更多内容关注irumor</font>
-            <img src="http://i.imgur.com/o8L9ItZ.jpg"/>
+            <br>
+            <img src="http://i.imgur.com/o8L9ItZ.jpg" width="100%"/>
             </body>
         </html>""" % (title, title, date, cti)
     return html_str
